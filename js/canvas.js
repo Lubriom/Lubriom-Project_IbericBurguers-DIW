f1eb7d3bd094ebcdc7fc5520486e5c5a32be5dfa
Lubriom <lunarmorer@gmail.com>
2025-02-09 19:55:36 +0100

    Se corrige ruta del canvas

--- js/canvas.js
+++ js/canvas.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let container = document.getElementById("canvasContainer");
 
   const imagenes = {
-    img1: "/rsc/sprites/burger1.png",
-    img2: "/rsc/sprites/burger2.png"
+    img1: "../../rsc/sprites/burger1.png",
+    img2: "../../rsc/sprites/burger2.png"
   };
 
   let ctx = canvas.getContext("2d");
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
   ];
 
   img.src = imagenes.img1;
-  star.src = "/rsc/sprites/star-spritesheet.png";
+  star.src = "../../rsc/sprites/star-spritesheet.png";
 
   function draw() {
     ctx.imageSmoothingEnabled = false;
